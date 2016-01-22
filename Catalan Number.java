2f7e657ca681c32948a9463b198ef83bdbe85b64
ice <1443700905@qq.com>
2016-01-22 20:01:48 +0800

    2016/1/22

---
+++ Catalan Number.java
@@ -0,0 +1,11 @@
+	Scanner input=new Scanner(System.in);
+	BigInteger a=new BigInteger("1");
+	BigInteger b=new BigInteger("2");
+	BigInteger c=new BigInteger("4");
+	BigInteger d=new BigInteger("1");
+	BigInteger ans[]=new BigInteger[10005];
+	ans[1]=a;
+	for(int i=2;i<=10000;i++){
+		d=d.add(a);
+		ans[i]=ans[i-1].multiply(c.multiply(d).subtract(b)).divide(d.add(a));
+	}
