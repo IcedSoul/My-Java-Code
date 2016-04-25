bedc54b824155e3c5142be9cfe3565d3f765b414
ice <1443700905@qq.com>
2016-04-25 21:09:31 +0800

    2016/4/25

--- CF/src/Main.java
+++ CF/src/Main.java
@@ -1,45 +1,38 @@
 import java.math.*;
-import java.io.*;
 import java.util.*;
 
 public class Main {
 	public static void main(String[] args){
-		try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
-            int a[]=new int[300005],num[]=new int[300005];
-            int n=br.read(),count=0;
-            num[0]=0;
-            for(int i=1;i<=n;i++){
-            	a[i]=br.read();
-            	out.write(i+" "+a[i]+"\n");
-                out.flush();
-            	for(int j=num[count]+1;j<i;j++){
-            		if(a[i]==a[j]){
-            			count++;
-            			num[count]=i;
-            		}
-            	}
-            }
-            out.write("lalal");
-            out.flush();
-            if(count==0){	
-            	out.write("-1");
-            	out.flush();
-            }
-            else
-            {
-            	out.println(count);
-            	out.flush();
-            	for(int i=0;i<count;i++){
-            		out.write((num[i]+1)+" "+num[i+1]+"\n");
-            		out.flush();
-            	}
-            	
-            	
-            }
-        } catch (IOException e) {
-        }
-		
+		Scanner input=new Scanner(System.in);
+		int count=0;
+		while(input.hasNext()){
+			String a=input.nextLine();
+			a=a.toUpperCase();
+			a=a.replaceAll("-"," ");
+			a=a.replaceAll(" THE "," ");
+			a=a.replaceAll(" OF "," ");
+			a=a.replaceAll(" AND "," ");
+			a=a.replaceAll(" FOR "," ");
+			a=a.replaceAll(" AN "," ");
+			a=a.replaceAll(" A "," ");
+			if(a.startsWith("THE "))
+				a=a.substring(4, a.length());
+			if(a.startsWith("OF "))
+				a=a.substring(3, a.length());
+			if(a.startsWith("FOR "))
+				a=a.substring(4, a.length());
+			if(a.startsWith("AND "))
+				a=a.substring(4, a.length());
+			if(a.startsWith("AN "))
+				a=a.substring(3, a.length());
+			if(a.startsWith("A "))
+				a=a.substring(2, a.length());
+			System.out.print("Case "+(++count)+": ");
+			System.out.print(a.charAt(0));
+			for(int i=1;i<a.length()-1;i++)
+				if(a.charAt(i)==' ')
+				System.out.print(a.charAt(i+1));
+			System.out.println();
+		}	
 	}
 }
